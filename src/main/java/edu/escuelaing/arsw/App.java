d729ec31cd049a52b52b2dadefd18735f145dcf9
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-06-17 09:27:20 -0500

    Heroku setup

--- src/main/java/edu/escuelaing/arsw/App.java
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -4,10 +4,8 @@ package edu.escuelaing.arsw;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
